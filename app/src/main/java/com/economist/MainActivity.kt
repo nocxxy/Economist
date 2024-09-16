d89f29d3a165b338189f06f1d9d70be13ac9e5b7
nocxxy <evan.meziere@gmail.com>
2024-09-16 17:40:24 +0200

    Init Main

--- app/src/main/java/com/economist/MainActivity.kt
+++ app/src/main/java/com/economist/MainActivity.kt
@@ -22,7 +22,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Text(text = "Hello World")
+
                 }
             }
         }
