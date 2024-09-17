454686d7c6107dfb52f8d97a254074a87e21cdf7
nocxxy <evan.meziere@gmail.com>
2024-09-17 19:26:56 +0200

    feat : color of the theme be add

--- app/src/main/java/com/economist/ui/theme/Color.kt
+++ app/src/main/java/com/economist/ui/theme/Color.kt
@@ -2,10 +2,8 @@ package com.economist.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
 
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
+val BackgroundColorEconomist = Color(0xFF023535)
+val PrimaryColorEconomist = Color(0xFF0CABA8)
+val SecondaryColorEconomist = Color(0xFF008F8C)
+val TertiaryColorEconomist = Color(0xFF0FC2C0)
--- app/src/main/java/com/economist/ui/theme/Theme.kt
+++ app/src/main/java/com/economist/ui/theme/Theme.kt
@@ -1,7 +1,6 @@
 package com.economist.ui.theme
 
 import android.app.Activity
-import android.os.Build
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.darkColorScheme
@@ -16,15 +15,17 @@ import androidx.compose.ui.platform.LocalView
 import androidx.core.view.WindowCompat
 
 private val DarkColorScheme = darkColorScheme(
-    primary = Purple80,
-    secondary = PurpleGrey80,
-    tertiary = Pink80,
+    primary = PrimaryColorEconomist,
+    secondary = SecondaryColorEconomist,
+    tertiary = TertiaryColorEconomist,
+    background = BackgroundColorEconomist,
 )
 
 private val LightColorScheme = lightColorScheme(
-    primary = Purple40,
-    secondary = PurpleGrey40,
-    tertiary = Pink40
+    primary = PrimaryColorEconomist,
+    secondary = SecondaryColorEconomist,
+    tertiary = TertiaryColorEconomist,
+    background = BackgroundColorEconomist,
 
     /* Other default colors to override
     background = Color(0xFFFFFBFE),
@@ -45,7 +46,7 @@ fun EconomistTheme(
     content: @Composable () -> Unit
 ) {
     val colorScheme = when {
-        dynamicColor && Build.VERSION.SDK_INT >= Build.VERSION_CODES.S -> {
+        dynamicColor && true -> {
             val context = LocalContext.current
             if (darkTheme) dynamicDarkColorScheme(context) else dynamicLightColorScheme(context)
         }
